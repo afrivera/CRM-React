97d118474b53a674e4e70b428edb6dffabab00e6
arivera <andresrfelip@hotmail.com>
2022-03-18 20:39:51 -0500

    Configuración componente layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 
 const Layout = () => {
+
+  const { pathname : urlActual } = useLocation();
+  
+  
+
   return (
-    <div>
-        <h1>Desde Layout</h1>
-        <Outlet />
+    <div className="md:flex md:min-h-screen">
+        <div className="md:w-1/4 bg-blue-900 px-5 py-10">
+          <h2 className="text-2xl font-black text-center text-white">CRM - Clientes</h2>
+
+          <nav>
+            <Link 
+              to="/clientes"
+              className={`${urlActual === '/clientes'? 'text-blue-300' : 'text-white '} text-2xl block mt-2 hover:text-blue-300`}
+            >Clientes</Link>
+            <Link 
+              to="/clientes/nuevo"
+              className={`${urlActual === '/clientes/nuevo'? 'text-blue-300' : 'text-white '} text-2xl block mt-2 hover:text-blue-300`}
+            >Nuevo Cliente</Link>
+          </nav>
+        </div>
+
+        <div className="md:w-3/4">
+          <Outlet />
+        </div>
     </div>
   )
 }
